f80d7023c87af3384aaeb2e692c50b5e701b20e6
Ruth Uwugiaren <ruth.uwugiaren@wsu.edu>
2023-01-17 12:38:53 -0800

    Newtest Update

--- Module1-Intro_to_Python/Assignment6.py
+++ Module1-Intro_to_Python/Assignment6.py
@@ -2,7 +2,7 @@ print ("Hello World!")
 
 from sys import argv
 script, arg1, arg2, arg3 = argv
-print ("This script is named {}".format(script_name))
-print (" The value of argument 1 is: {}", arg1)
-print (" The value of argument 2 is: {}", arg2)
-print (" The value of argument 3 is: {}", arg3)
+print ("This script is named {}".format(script))
+print (" The value of argument 1 is: {}".format(arg1))
+print (" The value of argument 2 is: {}".format(arg2))
+print (" The value of argument 3 is: {}".format(arg3))
